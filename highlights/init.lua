f8b19bc823c240a2cfbf48a12edb6d628620dce8
George Phillips <112098195+ItsGPhillips@users.noreply.github.com>
2023-04-15 11:55:29 +0100

    colorscheme

--- highlights/init.lua
+++ highlights/init.lua
@@ -1,28 +1,47 @@
 return {
   -- this table overrides highlights in all themes
-  Normal                = { bg = "#0d0d0d" },
-  ["@string"]           = { fg = "#d4ad52" },
-  ["@string.escape"]    = { fg = "#2165d1" },
-  ["@type"]             = { fg = "#92d15f" },
-  ["@attribute"]        = { fg = "#ba6cee" },
-  ["@operator"]         = { fg = "#d3d2c2" },
-  ["@number"]           = { fg = "#d0cc56" },
-  ["@boolean"]          = { fg = "#5388c0" },
-  ["@keyword.return"]   = { fg = "#bf8bba" },
-  ["@conditional"]      = { fg = "#bf8bba" },
-  ["@repeat"]           = { fg = "#bf8bba" },
-  ["@type.qualifier"]   = { fg = "#5d85db" },
-  ["@parameter"]        = { fg = "#81d6e6", style = "bold" },
-  ["@variable"]         = { fg = "#81d6e6" },
-  ["@constant.builtin"] = { fg = "#5d85db" },
-  ["@namespace"]        = { fg = "#4c7cec" },
-  ["@tag"]              = { fg = "#4c7cec" },
-  ["@tag.attribute"]    = { fg = "#81d6e6", style = "italic" }, -- Tags like html tag names.
-  ["@tag.delimiter"]    = { fg = "#d3d2c2" },
-  ["@field"]            = { fg = "#d5c96e" },
-  ["@property"]         = { link = "@field" },
-  ["@include"]          = { fg = "#5d8fdb" },
-  ["@keyword"]          = { fg = "#5d8fdb" },
-  ["@function"]         = { fg = "#d5c96e" },
-  ["@method"]           = { fg = "#d5c96e" },
+  Normal = { bg = "#0d0d0d" },
+  ["@none"] = { fg = "#dedede" },
+  ["@punctuation"] = { fg = "#d3d2c2" },
+  ["@punctuation.special"] = { fg = "#2165d1" },
+  --
+  ["@keyword"] = { fg = "#5d8fdb" },
+  ["@include"] = { link = "@keyword" },
+  ["@type.qualifier"] = { link = "@keyword" },
+  ["@constant.builtin"] = { link = "@keyword" },
+  ["@namespace"] = { link = "@keyword" },
+  ["@conditional"] = { fg = "#bf8bba" },
+  ["@keyword.return"] = { link = "@conditional" },
+  ["@repeat"] = { link = "@conditional" },
+  --
+  ["@function"] = { fg = "#d5c96e" },
+  ["@lsp.type.function"] = { link = "@function" },
+  ["@method"] = { link = "@function" },
+  --
+  --
+  Structure = { link = "@type" },
+  Type = { link = "@type" },
+  ["@type"] = { fg = "#92d15f" },
+  ["@type.builtin"] = { link = "@keyword" },
+  ["@lsp.type.namespace"] = { fg = "#2461bd" },
+  --
+  ["@string"] = { fg = "#d4ad52" },
+  ["@string.escape"] = { fg = "#2165d1" },
+  ["@number"] = { fg = "#d0cc56" },
+  ["@boolean"] = { fg = "#5388c0" },
+  --
+  ["@tag"] = { link = "@keyword" },
+  ["@tag.attribute"] = { fg = "#81d6e6", italic = true }, -- Tags like html tag names.
+  ["@tag.delimiter"] = { link = "@punctuation" },
+  --
+  ["@parameter"] = { fg = "#81d6e6" },
+  ["@variable"] = { fg = "#81d6e6" },
+  ["@lsp.typemod.variable.readonly"] = { link = "@variable" },
+  ["@lsp.typemod.variable.defaultLibrary"] = { fg = "#c76f7a" },
+  --
+  ["@attribute"] = { fg = "#ba6cee" },
+  ["@field"] = { fg = "#cfc682" },
+  ["@property"] = { link = "@field" },
+  ["@tag.attribute.tsx"] = { link = "@field", italic = true },
+  --
 }
--- plugins/user.lua
+++ plugins/user.lua
@@ -9,57 +9,55 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  -- {
+  --   "EdenEast/nightfox.nvim",
+  --   config = function()
+  --     require("nightfox").setup {
+  --       options = {
+  --         dim_inactive = false,
+  --       },
+  --       groups = {
+  --         -- https://github.com/EdenEast/nightfox.nvim/blob/main/lua/nightfox/group/modules/treesitter.lua
+  --         all = {
+  --           ["@string"]           = { fg = "#d4ad52" },
+  --           ["@string.escape"]    = { fg = "#0d5bd9" },
+  --           ["@type"]             = { fg = "#92d15f" },
+  --           ["@attribute"]        = { fg = "#ba6cee" },
+  --           ["@operator"]         = { fg = "#d3d2c2" },
+  --           ["@number"]           = { fg = "#d0cc56" },
+  --           ["@boolean"]          = { fg = "#5388c0" },
+  --           ["@keyword.return"]   = { fg = "#bf8bba" },
+  --           ["@conditional"]      = { fg = "#bf8bba" },
+  --           ["@repeat"]           = { fg = "#bf8bba" },
+  --           ["@type.qualifier"]   = { fg = "#5d85db" },
+  --           ["@parameter"]        = { fg = "#81d6e6", style = "bold" },
+  --           ["@variable"]         = { fg = "#81d6e6" },
+  --           ["@constant.builtin"] = { fg = "#5d85db" },
+  --           ["@namespace"]        = { fg = "#4c7cec" },
+  --           ["@tag"]              = { fg = "#4c7cec" },
+  --           ["@tag.attribute"]    = { fg = "#81d6e6", style = "italic" }, -- Tags like html tag names.
+  --           ["@tag.delimiter"]    = { fg = "#d3d2c2" },
+  --           ["@field"]            = { fg = "#d5c96e" },
+  --           ["@property"]         = { link = "@field" },
+  --           ["@include"]          = { fg = "#5d8fdb" },
+  --           ["@keyword"]          = { fg = "#5d8fdb" },
+  --           ["@function"]         = { fg = "#d5c96e" },
+  --           ["@method"]           = { fg = "#d5c96e" },
+  --         }
+  --       }
+  --     }
+  --   end,
+  --   event = "User AstroFile",
+  -- },
   {
-    "EdenEast/nightfox.nvim",
-    config = function()
-      require("nightfox").setup {
-        options = {
-          dim_inactive = false,
-        },
-        groups = {
-          -- https://github.com/EdenEast/nightfox.nvim/blob/main/lua/nightfox/group/modules/treesitter.lua
-          all = {
-            ["@string"]           = { fg = "#d4ad52" },
-            ["@string.escape"]    = { fg = "#0d5bd9" },
-            ["@type"]             = { fg = "#92d15f" },
-            ["@attribute"]        = { fg = "#ba6cee" },
-            ["@operator"]         = { fg = "#d3d2c2" },
-            ["@number"]           = { fg = "#d0cc56" },
-            ["@boolean"]          = { fg = "#5388c0" },
-            ["@keyword.return"]   = { fg = "#bf8bba" },
-            ["@conditional"]      = { fg = "#bf8bba" },
-            ["@repeat"]           = { fg = "#bf8bba" },
-            ["@type.qualifier"]   = { fg = "#5d85db" },
-            ["@parameter"]        = { fg = "#81d6e6", style = "bold" },
-            ["@variable"]         = { fg = "#81d6e6" },
-            ["@constant.builtin"] = { fg = "#5d85db" },
-            ["@namespace"]        = { fg = "#4c7cec" },
-            ["@tag"]              = { fg = "#4c7cec" },
-            ["@tag.attribute"]    = { fg = "#81d6e6", style = "italic" }, -- Tags like html tag names.
-            ["@tag.delimiter"]    = { fg = "#d3d2c2" },
-            ["@field"]            = { fg = "#d5c96e" },
-            ["@property"]         = { link = "@field" },
-            ["@include"]          = { fg = "#5d8fdb" },
-            ["@keyword"]          = { fg = "#5d8fdb" },
-            ["@function"]         = { fg = "#d5c96e" },
-            ["@method"]           = { fg = "#d5c96e" },
-          }
-        }
-      }
-    end,
+    "m-demare/hlargs.nvim",
+    requires = { "nvim-treesitter/nvim-treesitter" },
     event = "User AstroFile",
   },
   {
-    'm-demare/hlargs.nvim',
-    requires = { 'nvim-treesitter/nvim-treesitter' },
-    event = "User AstroFile",
-  },
-  {
-    'echasnovski/mini.nvim',
-    version = '*',
+    "echasnovski/mini.nvim",
+    version = "*",
     lazy = false,
-    config = function()
-      require("mini.move").setup()
-    end
-  }
+    config = function() require("mini.move").setup() end,
+  },
 }
